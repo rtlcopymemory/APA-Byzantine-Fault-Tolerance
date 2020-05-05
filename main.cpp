092b80464e5fe6f8191e3c25062495a96055b722
WolfenCLI <wolfencli@zohomail.eu>
2020-05-05 19:10:51 +0200

    Checks and adds runs

--- main.cpp
+++ main.cpp
@@ -7,13 +7,14 @@
 #include "cluster/cluster.h"
 
 int main(int argc, char *argv[]) {
-    if (argc < 3) {
-        printf("Error: Missing argument\nUsage: %s <total number of processes> <traitor processes>\n", argv[0]);
+    if (argc < 4) {
+        printf("Error: Missing argument\nUsage: %s <runs> <total number of processes> <traitor processes>\n", argv[0]);
         exit(1);
     }
 
     int processes = (int) strtol(argv[1], nullptr, 10);
     int traitors = (int) strtol(argv[2], nullptr, 10);
+    int runs = (int) strtol(argv[3], nullptr, 10);
     if (processes < (3 * traitors + 1)) {
         printf("It is impossible for the reliable processes to reach a consensus\n");
         exit(0);
@@ -21,6 +22,11 @@ int main(int argc, char *argv[]) {
 
     if (processes < 0 || traitors < 0) {
         printf("Invalid number of processes or traitors\n");
+        exit(1);
+    }
+
+    if (runs < 1) {
+        printf("Runs must be a positive number");
         exit(1);
     }
 
